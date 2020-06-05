153c69502a854c2a2b92a3f8da897b5bc21d8760
uharston <uharston@gmail.com>
2020-06-05 13:17:10 -0500

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -10,7 +10,7 @@ class InteractiveRecord
 
     def self.column_names 
       DB[:conn].results_as_hash = true
-     DB[:conn].execute("PRAGMA table_info('#{table_name}')").map {|column| column["name"]}.compact 
+      DB[:conn].execute("PRAGMA table_info('#{table_name}')").map {|column| column["name"]}.compact 
     end 
 
     def initialize(options={})
@@ -28,7 +28,8 @@ class InteractiveRecord
     #    a.join(', ')
     end 
 
-    def values_for_insert 
+    def values_for_insert
+   
          
         values = [] 
         self.class.column_names.each do |col_names|
@@ -41,17 +42,29 @@ class InteractiveRecord
      end 
 
      def save
-     
-         sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES #{values_for_insert}"
+       
+        sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
       
         DB[:conn].execute(sql)
-       
-        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
-        # a = DB[:conn].execute("SELECT * FROM students")
         
+        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
     end
 
-   #def self.find_by_name(name)
+   def self.find_by_name(name)
+    DB[:conn].execute("SELECT * FROM #{self.table_name} WHERE name = ?", [name])
+   end 
+
+   def self.find_by(attribute)
+    column_name = attribute.keys[0].to_s
+    value_name = attribute.values[0]
+
+    sql = <<-SQL
+      SELECT * FROM #{table_name}
+      WHERE #{column_name} = ?
+      SQL
+
+    DB[:conn].execute(sql, value_name);
+  end
 
 
   
